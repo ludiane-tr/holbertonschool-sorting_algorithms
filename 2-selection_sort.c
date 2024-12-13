0620645d52e274f6949b5d00b3928a76bd57b680
Iyed13tns <m@g.com>
2024-12-13 19:52:44 +0100

    holberton

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,18 +16,18 @@ void selection_sort(int *array, size_t size)
 
     for (i = 0; i < size - 1; i++)
     {
-              min_idx = i;
-              for (j = i + 1; j < size; j++)
-              {
-                    if (array[j] < array[min_idx])
-                        min_idx = j;
-              }
-                    if (min_idx != i)
-                      {
-                            temp = array[i];
-                            array[i] = array[min_idx];
-                            array[min_idx] = temp;
-                            print_array(array, size);
-                      }
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+            print_array(array, size);
+        }
     }
 }
